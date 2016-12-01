74280c15e4f63588bd3eea9c046644dc7a04fc9b
Wasabideveloper <tobias.blaser@hsr.ch>
2016-12-01 15:15:07 +0100

    Enhance test selector

--- angular/e2e/app.po.solution.ts
+++ angular/e2e/app.po.solution.ts
@@ -9,7 +9,16 @@ export class ZooPage {
     return element(by.css('app-root h1')).getText();
   }
 
+  /**
+   * Why do we pollute the template by a test-attribute
+   * <ul *ngFor="let animal of animals" data-test-animal-list>
+   * and use '[data-test-animal-list] > *' instead of 'app-root ul > li'?
+   *
+   * If we change the structure of the view (what happens frequently) the test will break.
+   * If we use a test specific attribute the test is less fragile and
+   * we remember on change of the view to adapt the test.
+   */
   getListItem(id) {
-	return element.all(by.css('app-root ul > li')).get(id).getText();
+	return element.all(by.css('[data-test-animal-list] > *')).get(id).getText();
   }
 }
